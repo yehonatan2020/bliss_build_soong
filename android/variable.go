1a771b1f79e09c8d9d979901434f0ce3e41dd5e3
Simon Shields <simon@lineageos.org>
2017-08-23 12:05:33 +1000

    soong: extend with bliss board flags
    Includes the following commit:
    
    Author: Simon Shields <simon@lineageos.org>
    Date:   Mon Sep 4 21:09:44 2017 +1000
    
        soong: explicitly specify name of Bliss variables struct
    
        Change-Id: Ic754b8a13658ddc40620f985581bd1754b780b15
    
    Change-Id: If127c9da6b57340ae57e0cf725151cdcc3c82ef3
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -20,6 +20,8 @@ import (
 	"runtime"
 	"strings"
 
+	"bliss/soong/android"
+
 	"github.com/google/blueprint/proptools"
 )
 
@@ -124,6 +126,9 @@ type variableProperties struct {
 			Static_libs  []string
 			Srcs         []string
 		}
+
+		// include Bliss variables
+		Bliss android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -287,6 +292,8 @@ type productVariables struct {
 	ProductHiddenAPIStubsTest   []string `json:",omitempty"`
 
 	TargetFSConfigGen []string `json:",omitempty"`
+
+	Bliss android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
