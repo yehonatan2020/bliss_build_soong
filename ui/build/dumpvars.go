546ddf9400a5dcb66d99fc74284aa11c43e15ebc
Jackeagle <jackeagle102@gmail.com>
2020-10-07 03:07:02 -0400

    dumpvars: Add Bliss Variant
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I219fc9995ca66aa3e2259f3ebf34debb4be0e39c

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -137,6 +137,7 @@ func dumpMakeVars(ctx Context, config Config, goals, vars []string, write_soong_
 // Variables to print out in the top banner
 var BannerVars = []string{
 	"BLISS_VERSION",
+	"BLISS_BUILD_VARIANT",
 	"TARGET_PRODUCT",
 	"TARGET_BUILD_VARIANT",
 	"TARGET_ARCH",
