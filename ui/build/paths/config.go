1d1a33dcf6f57830b5c777d48ab5520dc64ffb0f
hmtheboy154 <buingoc67@gmail.com>
2023-08-24 14:57:25 -0400

    Whitelist realpath
    It is being used by copy-firmware.sh script
    
    Change-Id: I6b3d6fdb2faf56a43b86f9cacf21e5dc5f0d8d9d
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -112,6 +112,7 @@ var Configuration = map[string]PathConfig{
     "python3.8": Allowed,
     "python3.9": Allowed,
 	"python3.10": Allowed,
+    "realpath": Allowed,
     "resize2fs": Allowed,
 	"rsync":   Allowed,
     "repo":    Allowed,
